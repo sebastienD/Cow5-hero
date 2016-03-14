5c90d7452922a79b345b5db96bc1023f4016cf73
sebastien DESCAMPS <sdescamps.ext@orange.com>
2016-03-14 13:47:51 +0100

    feat: add firrst navigation

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,8 +1,18 @@
-import {Page} from 'ionic-angular';
+import {Page, NavController, NavParams} from 'ionic-angular';
+import {HeroPage} from '../hero/hero';
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
 
+  constructor(private nav: NavController, private navParams: NavParams) {
+    this.nav = nav;
+    this.navParams = navParams;
+  }
+
+  goToNextPage() {
+    () => console.log('want to go to hero !')
+    this.nav.push(HeroPage);
+  }
 }
